436742d359d9f5b037191afc30a78fdc0b256338
akqjxx <liuer2595919@163.com>
2018-12-24 10:27:08 +0800

    11

--- microservicecloud-api/src/main/java/com/atguigu/springcloud/dto/DeptDTO.java
+++ microservicecloud-api/src/main/java/com/atguigu/springcloud/dto/DeptDTO.java
@@ -3,6 +3,8 @@ package com.atguigu.springcloud.dto;
 
 import java.sql.Timestamp;
 
+import javax.validation.constraints.NotNull;
+
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.experimental.Accessors;
@@ -18,10 +20,12 @@ public class DeptDTO implements java.io.Serializable{
 	 */
 	private static final long serialVersionUID = 2189714473576358800L;
 	
+	@NotNull
 	private Long deptno; // 主键
 	private String dname; // 部门名称
 	private String db_source;
 	private Timestamp createTime;
+	private String loginNo;
 	
 	
 
